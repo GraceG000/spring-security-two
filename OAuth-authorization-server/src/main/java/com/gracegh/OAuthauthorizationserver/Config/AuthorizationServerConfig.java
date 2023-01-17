e74b842e3c28cc17f2c8fe1b1bb7344f2cc83950
GraceG000 <graceghansah2000@gmail.com>
2023-01-17 17:11:03 +0000

    First Push

--- OAuth-authorization-server/src/main/java/com/gracegh/OAuthauthorizationserver/Config/AuthorizationServerConfig.java
+++ OAuth-authorization-server/src/main/java/com/gracegh/OAuthauthorizationserver/Config/AuthorizationServerConfig.java
@@ -1,8 +1,15 @@
 package com.gracegh.OAuthauthorizationserver.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
@@ -10,4 +17,11 @@ public class AuthorizationServerConfig {
 @Autowired
 private PasswordEncoder passwordEncoder;
 
+@Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception{
+      OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+            return http.formLogin(Customizer.withDefaults()).build();
+}
 }
