0b90319e935fe9284a3eae5d70c9ac10c73d2887
GraceG000 <graceghansah2000@gmail.com>
2023-01-17 16:48:57 +0000

    First Push

---
+++ OAuth-authorization-server/src/main/java/com/gracegh/OAuthauthorizationserver/Entity/User.java
@@ -0,0 +1,27 @@
+package com.gracegh.OAuthauthorizationserver.Entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity//this will enable the table to connect to the database...
+@Data//automating the creation of getters and setters...
+@Table(name = "tbl_user")
+public class User {
+
+    //making the id field an id...
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(length = 60)
+    private String password;
+
+    private String role;
+    private boolean enabled = false;
+}
